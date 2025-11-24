1f6b901750e4332f811088c979432f6422dc00cb
SiyeongLee <happyamong123@naver.com>
2025-11-24 14:38:45 +0900

    [Feat] Effect Manager 제작

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -36,8 +36,9 @@ public class PlayerController : MonoBehaviour
     private bool wasGrounded;                                   //이전 프레임에 땅 이었는지 
     private float attackTimer;
 
-    private bool isUIMode = false;                                  //UI 모드 설정 
-
+    private bool isUIMode = false;
+    //UI 모드 설정 
+    public GameObject effectPos;
     // Start is called before the first frame update
     void Start()
     {
@@ -88,6 +89,7 @@ public class PlayerController : MonoBehaviour
                 isLanding = true;
                 landingTimer = landingDuration;
                 Debug.Log("착지");
+                EffectManager.Instance.PlayEffect("착지이펙트", transform.position);
             }
         }
     }
@@ -122,6 +124,7 @@ public class PlayerController : MonoBehaviour
             if(animator != null)
             {
                 animator.SetTrigger("attackTrigger");
+                EffectManager.Instance.PlayEffectWithDelay("공격이펙트", effectPos.transform.position, Quaternion.identity, 0.5f, 1.0f);
             }
         }                
     }
